fd561b27049bd672aa13a1c7d85f4615e7b5ab2c
Dulatto <dulat.omarov@gmail.com>
2022-03-04 22:41:58 -0500

    todo list

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,8 @@ function App() {
   }
   return (
     <ul>
-      <TodoListItem todo={todos[0]}/>
-      <TodoListItem todo={todos[1]}/>
+     <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
     </ul>
   );
 }
