97933abeca478a7c25d3dd8e3d0badbdfe032b0a
Dulatto <dulat.omarov@gmail.com>
2022-03-05 23:14:07 -0500

    bootstrap 5

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const TodoListItem: React.FC<Props> = ({todo, toggleTodo}) => {
     return (
-        <li>
+        <li style={{ border: '1px solid rgba(0, 0, 0, 0.05)' }}>
              <label style={{ textDecoration: todo.complete ? 'line-through' : undefined }}>
             <input 
                type="checkbox" 
