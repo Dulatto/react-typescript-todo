c6f0e437a9f3df3a32d287badc6aef642da5987b
Dulatto <dulat.omarov@gmail.com>
2022-03-05 23:21:31 -0500

    bootstrap 5

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -8,20 +8,23 @@ export const AddTodoForm: React.FC<Props> = ({ addTodo }) => {
     const [text, setText] = useState('');
 
     return (
-        <form>
+        <form className='col-4 ms-2 mt-3 '>
+            <div className='input-group'>
         <input 
           type="text" 
+          className="form-control"
           value={text}
            onChange={(e) => {
           setText(e.target.value);
         }} />
-        <button 
+        <button className='btn btn-sm btn-secondary'
         type="submit"
         onClick={(e) => {
             e.preventDefault();
             addTodo(text);
             setText('');
           }}>Add Todo</button>
+          </div>
       </form>
     );
 };
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const TodoList: React.FC<Props> = ({todos, toggleTodo}) => {
     return (
-        <ul>
+        <ul className="col-4 ms-2 mt-2">
         {todos.map((todo) => (
           <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo} />
         ))}
